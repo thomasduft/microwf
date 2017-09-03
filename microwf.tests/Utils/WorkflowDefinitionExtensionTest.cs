49509d453bb99de7cf2b6a8e2641c9bdb39db808
Thomas Duft <thomas.duft@gmail.com>
2017-09-03 15:13:16 +0200

    Added WorkflowDefinitionExtensionTest

---
+++ microwf.tests/Utils/WorkflowDefinitionExtensionTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using microwf.tests.WorkflowDefinitions;
+using microwf.Utils;
+using System.Text;
+
+namespace microwf.tests.Utils
+{
+  [TestClass]
+  public class WorkflowDefinitionExtensionTest
+  {
+    [TestMethod]
+    public void ToDot_IsOnOffWorkflow_ReturnsCorrectDotDiagraph()
+    {
+      // Arrange
+      var onOffWorkflowDefinition = new OnOffWorkflow();
+
+      var expected = new StringBuilder();
+      expected.AppendLine("digraph OnOffWorkflow {");
+      expected.AppendLine("  rankdir = LR;");
+      expected.AppendLine("  On -> Off [ label = SwitchOff ];");
+      expected.AppendLine("  Off -> On [ label = SwitchOn ];");
+      expected.AppendLine("}");
+
+      // Act
+      var diagraph = onOffWorkflowDefinition.ToDot();
+
+      // Assert
+      Assert.IsNotNull(diagraph);
+      Assert.AreEqual(expected.ToString(), diagraph);
+    }
+  }
+}
--- microwf/Utils/WorkflowDefinitionExtension.cs
+++ microwf/Utils/WorkflowDefinitionExtension.cs
@@ -5,14 +5,21 @@ namespace microwf.Utils
 {
   public static class WorkflowDefinitionExtension
   {
-    public static string ToDot(this IWorkflowDefinition workflow)
+    /// <summary>
+    /// Exports the workflow definition to a dot diagraph.
+    /// See: http://www.webgraphviz.com/
+    /// </summary>
+    /// <param name="workflow"></param>
+    /// <returns></returns>
+    public static string ToDot(this IWorkflowDefinition workflow, string rankDir = "LR")
     {
       var sb = new StringBuilder();
 
-      sb.AppendLine($"diagraph {workflow.WorkflowType} {{");
+      sb.AppendLine($"digraph {workflow.WorkflowType} {{");
+      sb.AppendLine($"  rankdir = {rankDir};");
       foreach(var t in workflow.Transitions)
       {
-        sb.AppendLine($"{t.State} -> {t.TargetState} [label={t.Trigger}]");
+        sb.AppendLine($"  {t.State} -> {t.TargetState} [ label = {t.Trigger} ];");
       }
       sb.AppendLine("}");
 
