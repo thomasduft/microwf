ba5dab145dbd69dc095a409e6e46848fb10b4b03
Thomas Duft <thomas.duft@gmail.com>
2017-11-28 19:49:49 +0100

    Some punctuation in comments

--- microwf/Execution/TransitionContext.cs
+++ microwf/Execution/TransitionContext.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 namespace microwf.Execution
 {
   /// <summary>
-  /// Provides information about the trigger to be executed
+  /// Provides information about the trigger to be executed.
   /// </summary>
   public class TransitionContext
   {
@@ -13,7 +13,7 @@ namespace microwf.Execution
     private List<string> _errors;
 
     /// <summary>
-    /// Indicates whether variables are available
+    /// Indicates whether variables are available.
     /// </summary>
     public bool HasVariables
     {
@@ -21,7 +21,7 @@ namespace microwf.Execution
     }
 
     /// <summary>
-    /// Indicates whether a transition should be aborted
+    /// Indicates whether a transition should be aborted.
     /// </summary>
     public bool TransitionAborted { get; private set; }
 
@@ -31,7 +31,7 @@ namespace microwf.Execution
     public IWorkflow Workflow { get; private set; }
 
     /// <summary>
-    /// Returns a list of error messages if during triggering some validation failed
+    /// Returns a list of error messages if during triggering some validation failed.
     /// </summary>
     public IEnumerable<string> Errors
     {
@@ -46,7 +46,7 @@ namespace microwf.Execution
     }
 
     /// <summary>
-    /// Returns the typed workflow that you expect
+    /// Returns the typed workflow that you expect.
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <returns></returns>
@@ -56,7 +56,7 @@ namespace microwf.Execution
     }
 
     /// <summary>
-    /// Checks whether a key is available for the variables dictionary
+    /// Checks whether a key is available for the variables dictionary.
     /// </summary>
     /// <param name="key"></param>
     /// <returns></returns>
@@ -68,7 +68,7 @@ namespace microwf.Execution
     }
 
     /// <summary>
-    /// Sets a workflow variable
+    /// Sets a workflow variable.
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <param name="key"></param>
@@ -86,7 +86,7 @@ namespace microwf.Execution
     }
 
     /// <summary>
-    /// Gets a workflow variable by its key
+    /// Gets a workflow variable by its key.
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <param name="key"></param>
@@ -100,7 +100,7 @@ namespace microwf.Execution
     }
 
     /// <summary>
-    /// Adds an error message
+    /// Adds an error message.
     /// Note: Use it to do validation.
     /// </summary>
     /// <param name="message"></param>
@@ -110,7 +110,7 @@ namespace microwf.Execution
     }
 
     /// <summary>
-    /// Stops the current transition to be done
+    /// Stops the current transition to be done.
     /// Note: this can only be called before the transition is done.
     /// </summary>
     public void AbortTransition(string reason)
