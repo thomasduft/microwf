f7c95ecade11d27e161088c7bdc28b4569a4893a
Duft Thomas <thomas.duft@buhlergroup.com>
2018-03-14 15:39:03 +0100

    returned all registered WorkflowDefinitions in ValuesController

--- samples/WebApi/Controllers/ValuesController.cs
+++ samples/WebApi/Controllers/ValuesController.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using tomware.Microwf;
 
 namespace WebApi.Controllers
 {
   [Route("api/[controller]")]
   public class ValuesController : Controller
   {
+    private readonly IServiceProvider _serviceProvider;
+
+    public ValuesController(IServiceProvider serviceProvider) {
+      _serviceProvider = serviceProvider;
+    }
+
     // GET api/values
     [HttpGet]
     public IEnumerable<string> Get()
     {
-      return new string[] { "value1", "value2" };
+      var workflowDefinitions = this._serviceProvider.GetServices<IWorkflowDefinition>();
+      
+      return workflowDefinitions.Select(d => d.WorkflowType);
     }
 
     // GET api/values/5
