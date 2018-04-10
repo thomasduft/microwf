71afc5b5589104b2e72eb9d29488b0d1272c7dd5
Thomas Duft <thomas.duft@gmail.com>
2018-04-10 22:46:38 +0200

    addes build.sh script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -z "$1" ] 
+then
+  echo No version specified! Please specify a valid version like 1.2.3!
+  exit 1
+else
+  echo version $1
+fi
+
+echo Restore solution
+dotnet restore microwf.sln
+
+echo Packaging solution
+dotnet pack src/microwf.Core -c Release /p:PackageVersion=$1 -o ./../../dist/nupkgs
+
+echo Done
