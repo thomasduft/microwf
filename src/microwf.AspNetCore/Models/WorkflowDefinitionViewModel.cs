837ac0c0c6cde0ccb3970615106ba7446bb2cc07
Thomas Duft <thomas.duft@gmail.com>
2018-04-10 22:21:01 +0200

    removed AspNetCore project

--- src/microwf.AspNetCore/Models/WorkflowDefinitionViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace tomware.Microwf.AspNetCore
-{
-  public class WorkflowDefinitionViewModel
-  {
-    public string Type { get; set; }
-    public string StartUrl { get; set; }
-    public string Description { get; set; }
-  }
-}
