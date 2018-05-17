b96090d6ac9316fb6e01adaca117b699bbb96524
Thomas Duft <thomas.duft@gmail.com>
2018-05-17 22:46:56 +0200

    upgraded to latest angular and added dummy test

---
+++ samples/WebClient/src/app/app.spec.ts
@@ -0,0 +1,5 @@
+describe('jasmine', () => {
+  it('should be running', () => {
+    expect(true).toBeTruthy();
+  });
+});
--- samples/WebClient/src/karma.conf.js
+++ samples/WebClient/src/karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-spec-reporter'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
@@ -20,12 +21,12 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['spec'], // kjhtml
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false
   });
 };
