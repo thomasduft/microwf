403a95c84bf92d419c2fb78bd519df9fe4d452a2
Thomas Duft <thomas.duft@outlook.com>
2019-03-28 11:45:30 +0100

    removed commented code

--- test/Utils/SimpleWorkflowDefinitionProvider.cs
+++ test/Utils/SimpleWorkflowDefinitionProvider.cs
@@ -44,41 +44,4 @@ namespace microwf.Tests.Utils
       return _workflowDefinitions;
     }
   }
-
-  //public class WebWorkflowDefinitionProvider : IWorkflowDefinitionProvider
-  //{
-  //  private readonly IServiceProvider _serviceProvider;
-
-  //  public WebWorkflowDefinitionProvider(IServiceProvider serviceProvider)
-  //  {
-  //    _serviceProvider = serviceProvider;
-  //  }
-
-  //  public void RegisterWorkflowDefinition(IWorkflowDefinition workflowDefinition)
-  //  {
-  //    /**
-  //    * registering done via di framework during Startup.ConfigureServices
-  //    * public void ConfigureServices(IServiceCollection services)
-  //    * 
-  //    * Sample:
-  //    *  ...
-  //    *  services.AddMvc();
-  //    *
-  //    *  services.AddSingleton<IWorkflowDefinitionProvider, WebWorkflowDefinitionProvider>();
-  //    *  services.AddTransient<ILoggerService, LoggerService>();
-  //    *  services.AddTransient<IWorkflowDefinition, OnOffWorkflow>();
-  //    *  services.AddTransient<IWorkflowDefinition, HolidayApprovalWorkflow>();
-  //    *  ...
-  //    *  
-  //    */
-
-  //    throw new NotImplementedException();
-  //  }
-
-  //  public IWorkflowDefinition GetWorkflowDefinition(string type)
-  //  {
-  //    return _serviceProvider
-  //      .GetServices<IWorkflowDefinition>().First(t => t.WorkflowType == type);
-  //  }
-  //}
 }
