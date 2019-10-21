1c05d78729d262f8311ddc5e3b4c4d53da459525
thomasduft <thomas.duft@gmail.com>
2019-10-21 10:13:03 +0200

    removed direct access to DbContext in WorkItemRepository

--- src/microwf.AspNetCoreEngine/Infrastructure/Data/WorkItemRepository.cs
+++ src/microwf.AspNetCoreEngine/Infrastructure/Data/WorkItemRepository.cs
@@ -17,26 +17,31 @@ namespace tomware.Microwf.Engine
     {
       var ids = items.Select(wi => wi.Id).ToArray();
 
-      var existingItems = await this.DbContext.WorkItems
-        .Where(wi => ids.Contains(wi.Id))
-        .ToListAsync<WorkItem>();
-
+      var existingItems = await this.ListAsync(new WorkItemFilterForExistingItems(ids));
       var comparer = new WorkItemComparer();
 
       // updates
       var updates = items.Intersect(existingItems, comparer);
-      this.DbContext.WorkItems.UpdateRange(updates);
+      // this.DbContext.WorkItems.UpdateRange(updates);
+      foreach (var update in updates)
+      {
+        await this.UpdateAsync(update);
+      }
 
       // new items
       var inserts = items.Except(existingItems, comparer);
-      this.DbContext.WorkItems.AddRange(inserts);
+      // this.DbContext.WorkItems.AddRange(inserts);
+      foreach (var insert in inserts)
+      {
+        await this.AddAsync(insert);
+      }
 
-      await this.DbContext.SaveChangesAsync();
+      // await this.DbContext.SaveChangesAsync();
     }
 
     public async Task Reschedule(WorkItemInfoViewModel model)
     {
-      var item = await this.DbContext.WorkItems.FindAsync(model.Id);
+      var item = await this.GetByIdAsync(model.Id);
 
       item.Retries = Constants.WORKITEM_RETRIES; // so it reschedules only once!
       if (model.DueDate.HasValue)
@@ -44,9 +49,7 @@ namespace tomware.Microwf.Engine
         item.DueDate = model.DueDate.Value;
       }
 
-      this.DbContext.WorkItems.Update(item);
-
-      await this.DbContext.SaveChangesAsync();
+      await this.UpdateAsync(item);
     }
 
     public async Task RemoveAsync(int id)
