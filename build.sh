03b6ac93567a2f257dd22ebee6023bc48fff40b5
thomasduft <thomas.duft@gmail.com>
2020-08-25 07:35:45 +0200

    added push to nuget possibility for build.sh

--- build.sh
+++ build.sh
@@ -8,13 +8,28 @@ else
   echo version $1
 fi
 
+echo Cleaning up
+rm -r ./dist
+
 echo Restore solution
 dotnet restore microwf.sln
 
-echo Packaging solution
+version = $1
+echo Packaging solution with PackageVersion = $1 and Version = version
 dotnet pack src/microwf.Core -c Release -p:PackageVersion=$1 -p:Version=$1 -o ./dist/nupkgs/
 dotnet pack src/microwf.Domain -c Release -p:PackageVersion=$1 -p:Version=$1 -o ./dist/nupkgs/
 dotnet pack src/microwf.Infrastructure -c Release -p:PackageVersion=$1 -p:Version=$1 -o ./dist/nupkgs/
 dotnet pack src/microwf.AspNetCoreEngine -c Release -p:PackageVersion=$1 -p:Version=$1 -o ./dist/nupkgs/
+
+if [ -z "$2" ]
+then
+  echo Done
+  exit 0
+fi
+
+for package in $(find ./dist/nupkgs/ -name *.nupkg); do
+  echo Pushing $package
+  dotnet nuget push $package -k $2 -s https://api.nuget.org/v3/index.json
+done
 
 echo Done
