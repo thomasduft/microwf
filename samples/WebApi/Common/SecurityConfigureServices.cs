2ef08b88f5a3cea5aac1343d02fe8018a796ffc9
thomasduft <thomas.duft@gmail.com>
2022-10-31 21:43:12 +0000

    removed unused code in ConsoleClient and fixed authentication for StepperController

--- samples/WebApi/Common/SecurityConfigureServices.cs
+++ samples/WebApi/Common/SecurityConfigureServices.cs
@@ -1,8 +1,4 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.Net.Http.Headers;
-using OpenIddict.Server.AspNetCore;
 using static OpenIddict.Abstractions.OpenIddictConstants;
 
 namespace WebApi;
@@ -28,52 +24,6 @@ public static class CommonConfigureServices
       options.ClaimsIdentity.UserIdClaimType = Claims.Subject;
       options.ClaimsIdentity.RoleClaimType = Claims.Role;
     });
-
-    services
-      .AddAuthentication(options =>
-      {
-        // custom scheme defined in .AddPolicyScheme() below
-        options.DefaultScheme = "JWT_OR_COOKIE";
-        options.DefaultChallengeScheme = "JWT_OR_COOKIE";
-      })
-      // .AddCookie(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme, options =>
-      // {
-      //   options.LoginPath = "/login";
-      //   options.LogoutPath = "/logout";
-      //   options.ExpireTimeSpan = TimeSpan.FromDays(1);
-      // })
-      .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
-      {
-        // options.RequireHttpsMetadata = false;
-        // options.IncludeErrorDetails = true;
-        // options.SaveToken = true;
-        // options.MapInboundClaims = false;
-        options.TokenValidationParameters = new TokenValidationParameters()
-        {
-          ValidateIssuer = false,
-          ValidateAudience = false,
-          NameClaimType = Claims.Name,
-          RoleClaimType = Claims.Role
-        };
-      })
-      // see here: https://weblog.west-wind.com/posts/2022/Mar/29/Combining-Bearer-Token-and-Cookie-Auth-in-ASPNET
-      .AddPolicyScheme("JWT_OR_COOKIE", "JWT_OR_COOKIE", options =>
-      {
-        // runs on each request
-        options.ForwardDefaultSelector = context =>
-        {
-          // filter by auth type
-          string authorization = context.Request.Headers[HeaderNames.Authorization];
-          if (!string.IsNullOrEmpty(authorization)
-            && authorization.StartsWith($"{JwtBearerDefaults.AuthenticationScheme} "))
-          {
-            return JwtBearerDefaults.AuthenticationScheme;
-          }
-
-          // otherwise always check for cookie auth
-          return OpenIddictServerAspNetCoreDefaults.AuthenticationScheme;
-        };
-      });
 
     services
       .ConfigureApplicationCookie(options =>
