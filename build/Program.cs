2e3978bb6d6e6d9a1a70f73e47f4a1a2be70d76e
Thomas Duft <thomas.duft@buhlergroup.com>
2022-12-06 12:59:07 +0100

    improved build/Program.cs

--- build/Program.cs
+++ build/Program.cs
@@ -223,4 +223,29 @@ internal static class Program
 
     return files;
   }
+
+  static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+  {
+    var dir = new DirectoryInfo(sourceDir);
+    if (!dir.Exists)
+      throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+    DirectoryInfo[] dirs = dir.GetDirectories();
+    Directory.CreateDirectory(destinationDir);
+
+    foreach (FileInfo file in dir.GetFiles())
+    {
+      string targetFilePath = Path.Combine(destinationDir, file.Name);
+      file.CopyTo(targetFilePath);
+    }
+
+    if (recursive)
+    {
+      foreach (DirectoryInfo subDir in dirs)
+      {
+        string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+        CopyDirectory(subDir.FullName, newDestinationDir, true);
+      }
+    }
+  }
 }
